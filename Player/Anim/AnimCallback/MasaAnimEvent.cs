7bd53f422ae5fd4ebd93db0db6e644c0af5a39f7
moiling <super8moi@gmail.com>
2018-04-02 21:49:15 +0800

    Shader

--- Player/Anim/AnimCallback/MasaAnimEvent.cs
+++ Player/Anim/AnimCallback/MasaAnimEvent.cs
@@ -3,7 +3,11 @@
 
 public class MasaAnimEvent : MonoBehaviour {
     public AudioClip AttackAudio;
-    
+    /*
+    private void Start() {
+        GetComponent<SpriteRenderer>().material.shader = Shader.Find("Sprites/MasaShader");
+    }*/
+
     public void PlayAttackAudio() {
         AudioSource.PlayClipAtPoint(AttackAudio, FindObjectOfType<AudioListener>().transform.position, 100);
     }
